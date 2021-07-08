4987a26faa1c46b10ab7fc37a0e023bf65f3d641
Eitan Haibi <eitanhaibi55@gmail.com>
2021-07-08 17:25:15 +0300

    origin

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+#objects 
+ from objects import *
+
+#number
+
+ev3.speaker.beep(100, 3000)
+drive.straight(5)
+dirve.turn(60)
+
+#angle
+from angle import *
